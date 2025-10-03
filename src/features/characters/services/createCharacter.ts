65c53061a03d17be41734d537f5490570c4e7687
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-03 14:27:04 +0200

    feat: with backend validation

--- src/features/characters/services/createCharacter.ts
+++ src/features/characters/services/createCharacter.ts
@@ -1,9 +1,14 @@
 "use server";
 
 import db from "@/shared/lib/db";
-import { CreateCharacterDto } from "../contracts/characters.dto";
+import {
+  CreateCharacterDto,
+  createCharacterSchema,
+} from "../contracts/characters.dto";
 
 export const createCharacter = async (data: CreateCharacterDto) => {
+  createCharacterSchema.parse(data);
+
   return await db.character.create({
     data,
   });
