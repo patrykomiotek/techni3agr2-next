f8a5aa5b1bf54cf513c17193496d1612167740a4
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-24 13:04:50 +0200

    feat: with configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import { headers } from "next/headers";
 import Link from "next/link";
 
-export default function Home() {
+import { auth } from "@/shared/lib/auth";
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  console.log({ session });
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
--- src/shared/lib/auth.ts
+++ src/shared/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 import db from "@/shared/lib/db";
 
@@ -10,4 +11,5 @@ export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  plugins: [nextCookies()], // make sure this is the last plugin in the array
 });
